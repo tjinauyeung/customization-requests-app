0b7371543f49af52484e8fb22413b20f7e56ab80
Tjin Au Yeung <tjin@usabilla.com>
2016-10-28 17:22:01 +0200

    Configured React for project

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,30 @@ var path = require('path');
 
 module.exports = {
   entry: [
-    './app/app.js',
+    './app/index.js',
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
+    loaders: [
+    {
+      test: /\.(js|jsx)$/,
       loaders: ['babel-loader'],
       exclude: /node_modules/
-    }, {
+    },
+    {
       test: /\.css$/,
-      loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
-    }, {
-      test: /\.png|\.svg$/,
+      loader: 'style!css?importLoaders=1!postcss'
+    },
+    {
+      test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
       loaders: ['file-loader']
+    },
+    {
+      test: /\.scss$/,
+      loaders: ["style", "css", "sass"]
     }]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./assets/scss")]
   },
   output: {
     path: path.resolve(__dirname, 'build'),
